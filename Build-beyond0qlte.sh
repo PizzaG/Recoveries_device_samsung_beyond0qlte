8ec1a50394f49477d21f5406f1447470429887b3
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-04-21 23:48:55 -0500

    Add files via upload

---
+++ Build-beyond0qlte.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## RedWolf
+#############################
+make clean
+ccache -M 50G
+export USE_CCACHE=1
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL=C
+#export BR_MAINTAINER=PizzaG
+. build/envsetup.sh
+lunch omni_beyond0qlte-eng
+mka recoveryimage
+echo
+echo " Recovery Should Be Built"
+echo ""
+mv $OUT/RedWolf-PizzaG-beyond0qlte.img /media/pizzag/Android/Uploads/Recoveries/RedWolf_Recovery-beyond0qlte.img
+mv $OUT/RedWolf-PizzaG-beyond0qlte.img.md5 /media/pizzag/Android/Uploads/Recoveries/RedWolf_Recovery-beyond0qlte.img.md5
+mv $OUT/recovery.tar /media/pizzag/Android/Uploads/Recoveries/RedWolf_Recovery-beyond0qlte.tar
+cd /media/pizzag/Android/Recoveries/RedWolf/8
+make clean
+#############################
+#############################
+
